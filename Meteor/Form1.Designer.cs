5dd2499ebf6e397af3424ecdb0a24d2a3357d2ba
yanzhen0923 <yanzhen0923@gmail.com>
2014-11-11 21:47:15 +0800

    v1.0.0

---
+++ Meteor/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Meteor
+{
+    partial class 一起去看流星雨
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(一起去看流星雨));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.菜单 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.控制ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.流星雨ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.换个方向ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.让流星雨来得更猛烈些吧ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.恢复原状ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.小星星ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.看看满天繁星ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.恢复原状ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.菜单.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipText = " 一起去看流星雨吧~";
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "一起去看流星雨吧~";
+            this.notifyIcon.Visible = true;
+            // 
+            // 菜单
+            // 
+            this.菜单.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.控制ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.菜单.Name = "contextMenuStrip1";
+            this.菜单.Size = new System.Drawing.Size(99, 48);
+            this.菜单.Text = "菜单";
+            // 
+            // 控制ToolStripMenuItem
+            // 
+            this.控制ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.流星雨ToolStripMenuItem,
+            this.小星星ToolStripMenuItem});
+            this.控制ToolStripMenuItem.Name = "控制ToolStripMenuItem";
+            this.控制ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.控制ToolStripMenuItem.Text = "控制";
+            // 
+            // 流星雨ToolStripMenuItem
+            // 
+            this.流星雨ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.换个方向ToolStripMenuItem,
+            this.让流星雨来得更猛烈些吧ToolStripMenuItem,
+            this.恢复原状ToolStripMenuItem});
+            this.流星雨ToolStripMenuItem.Name = "流星雨ToolStripMenuItem";
+            this.流星雨ToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.流星雨ToolStripMenuItem.Text = " 流星雨";
+            // 
+            // 换个方向ToolStripMenuItem
+            // 
+            this.换个方向ToolStripMenuItem.Name = "换个方向ToolStripMenuItem";
+            this.换个方向ToolStripMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.换个方向ToolStripMenuItem.Text = "换个方向";
+            this.换个方向ToolStripMenuItem.Click += new System.EventHandler(this.换个方向ToolStripMenuItem_Click);
+            // 
+            // 让流星雨来得更猛烈些吧ToolStripMenuItem
+            // 
+            this.让流星雨来得更猛烈些吧ToolStripMenuItem.Name = "让流星雨来得更猛烈些吧ToolStripMenuItem";
+            this.让流星雨来得更猛烈些吧ToolStripMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.让流星雨来得更猛烈些吧ToolStripMenuItem.Text = "让流星雨来得更猛烈些吧~";
+            this.让流星雨来得更猛烈些吧ToolStripMenuItem.Click += new System.EventHandler(this.让流星雨来得更猛烈些吧ToolStripMenuItem_Click);
+            // 
+            // 恢复原状ToolStripMenuItem
+            // 
+            this.恢复原状ToolStripMenuItem.Name = "恢复原状ToolStripMenuItem";
+            this.恢复原状ToolStripMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.恢复原状ToolStripMenuItem.Text = "恢复原状";
+            this.恢复原状ToolStripMenuItem.Click += new System.EventHandler(this.恢复原状ToolStripMenuItem_Click);
+            // 
+            // 小星星ToolStripMenuItem
+            // 
+            this.小星星ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.看看满天繁星ToolStripMenuItem,
+            this.恢复原状ToolStripMenuItem1});
+            this.小星星ToolStripMenuItem.Name = "小星星ToolStripMenuItem";
+            this.小星星ToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.小星星ToolStripMenuItem.Text = "小星星";
+            // 
+            // 看看满天繁星ToolStripMenuItem
+            // 
+            this.看看满天繁星ToolStripMenuItem.Name = "看看满天繁星ToolStripMenuItem";
+            this.看看满天繁星ToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.看看满天繁星ToolStripMenuItem.Text = "看看满天繁星";
+            this.看看满天繁星ToolStripMenuItem.Click += new System.EventHandler(this.看看满天繁星ToolStripMenuItem_Click);
+            // 
+            // 恢复原状ToolStripMenuItem1
+            // 
+            this.恢复原状ToolStripMenuItem1.Name = "恢复原状ToolStripMenuItem1";
+            this.恢复原状ToolStripMenuItem1.Size = new System.Drawing.Size(146, 22);
+            this.恢复原状ToolStripMenuItem1.Text = "恢复原状";
+            this.恢复原状ToolStripMenuItem1.Click += new System.EventHandler(this.恢复原状ToolStripMenuItem1_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 26);
+            // 
+            // 一起去看流星雨
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1200, 700);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "一起去看流星雨";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "一起去看流星雨";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.一起去看流星雨_Load);
+            this.菜单.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip 菜单;
+        private System.Windows.Forms.ToolStripMenuItem 控制ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 流星雨ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 小星星ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 换个方向ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 让流星雨来得更猛烈些吧ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 恢复原状ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 看看满天繁星ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 恢复原状ToolStripMenuItem1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Meteor/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Meteor
+{
+    public partial class 一起去看流星雨 : Form
+    {
+        [DllImport("winmm.dll")]
+        public static extern int mciSendString(string m_strCmd, string m_strReceive, int m_v1, int m_v2);
+        private double Angle = 2 * Math.PI * ((double)55 / (double)360);
+        int CurrentMeterors = 0;
+        int CurrentStars = 0;
+        int MaxMeteors = 24;
+        int MaxStars = 32;
+        bool ClockWise0 = false;
+        bool Stop = false;
+ 
+        private void SingleMeteor()
+        {
+            Random random = new Random();
+            bool ClockWise = ClockWise0;
+            Graphics g = this.CreateGraphics();
+            int Thickness = random.Next(1, 5);
+            int StartX = random.Next(- 100, Screen.PrimaryScreen.WorkingArea.Width + 100);
+            int StartY = random.Next(0, Screen.PrimaryScreen.WorkingArea.Height >> 3);
+            int XLenth = random.Next(8, 60);
+            int EndX = ClockWise == true ? (StartX - XLenth) : (StartX + XLenth);
+            double YLenth = XLenth * Math.Tan(Angle);
+            int EndY = StartY + (int)YLenth;
+            int SleepInterval = random.Next(2, 30);
+            Pen p = new Pen(Color.FromArgb(random.Next(150, 256), random.Next(150, 256), random.Next(150, 256)), Thickness);
+            Pen p0 = new Pen(BackColor, Thickness);
+            Point p1 = new Point(StartX, StartY);
+            Point p2 = new Point(EndX, EndY);
+            YLenth = 4 * Math.Tan(Angle);
+            for (; p1.Y <= Screen.PrimaryScreen.WorkingArea.Height; )
+            {
+                if (Stop == false)
+                {
+                    p0.Color = BackColor;
+                    g.DrawLine(p0, p1, p2);
+                    p1.X -= ClockWise == true ? 4 : -4;
+                    p2.X -= ClockWise == true ? 4 : -4;
+                    p1.Y += (int)(YLenth);
+                    p2.Y += (int)(YLenth);
+                    g.DrawLine(p, p1, p2);
+                }
+                else
+                    break;
+                Thread.Sleep(SleepInterval);
+            }
+            if (Stop == true)
+                return;
+            p0.Color = BackColor;
+            g.DrawLine(p0, p1, p2);
+            CurrentMeterors -= 1;
+            return;
+        }
+
+        private void SingleStar()
+        {
+            Random rd = new Random();
+            bool ExpandFlag = true;
+            int LifeTime = 20;
+            int rgb = rd.Next(1, 8);
+            int MaxSize = rd.Next(3, 7);
+            if (MaxSize == 3)
+                LifeTime = 10;
+            else if (MaxSize == 4)
+                LifeTime = 15;
+            else if (MaxSize == 5)
+                LifeTime = 20;
+            else if (MaxSize == 6)
+                LifeTime = 35;
+            MaxSize <<= 1;
+            MaxSize |= 1;
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddEllipse(-(MaxSize >> 1), -(MaxSize >> 1 ), MaxSize,MaxSize);
+            PathGradientBrush pthGrBrush = new PathGradientBrush(gp);
+            pthGrBrush.CenterColor = Color.FromArgb(255, 255, 255);
+            Color[] colors = { Color.FromArgb(255, rd.Next(100,200),rd.Next(100,200), rd.Next(100,200)) };
+            if (rgb == 1)
+                colors[0] = Color.FromArgb(rd.Next(20,80), rd.Next(200,256), rd.Next(200,256));
+            else if(rgb == 2)
+                colors[0] = Color.FromArgb(rd.Next(200, 256), rd.Next(20, 80), rd.Next(200, 256));
+            else if(rgb == 3)
+                colors[0] = Color.FromArgb(rd.Next(200, 256), rd.Next(200, 256), rd.Next(20, 80));
+            else if(rgb == 4)
+                colors[0] = Color.FromArgb(rd.Next(200, 256), rd.Next(20, 80), rd.Next(20, 80));
+            else if(rgb == 5)
+                colors[0] = Color.FromArgb(rd.Next(20, 80), rd.Next(200, 256), rd.Next(20, 80));
+            else if(rgb == 6)
+                colors[0] = Color.FromArgb(rd.Next(20, 80), rd.Next(20, 80), rd.Next(200, 256));
+            pthGrBrush.SurroundColors = colors;
+            SolidBrush sb0 = new SolidBrush(BackColor);
+            Graphics g = this.CreateGraphics();
+            g.TranslateTransform(rd.Next(10, Screen.PrimaryScreen.WorkingArea.Width - 100), rd.Next(10, Screen.PrimaryScreen.WorkingArea.Height));
+            float LastWidth = MaxSize;
+            float LastHeight = MaxSize;
+            ExpandFlag = false;
+            for (; ; )
+            {
+                if (Stop == false)
+                {
+                    sb0.Color = BackColor;
+                    g.FillEllipse(sb0, -((int)LastWidth >> 1), -((int)LastHeight >> 1), LastWidth, LastHeight);
+                    if (LifeTime <= 0)
+                        break;
+                    if (LastHeight == 3)
+                        ExpandFlag = true;
+                    else if (LastHeight == MaxSize)
+                        ExpandFlag = false;
+                    if (ExpandFlag == false)
+                    {
+                        LastHeight -= 2;
+                        LastWidth -= 2;
+                    }
+                    else
+                    {
+                        LastHeight += 2;
+                        LastWidth += 2;
+                    }
+                    g.FillEllipse(pthGrBrush, -((int)LastWidth >> 1), -((int)LastHeight >> 1), LastWidth, LastHeight);
+                }
+                else
+                    break;
+                if (LastWidth == MaxSize)
+                    Thread.Sleep(rd.Next(1300, 1700));
+                else if (LastWidth == 3)
+                    Thread.Sleep(rd.Next(900, 1300));
+                if (ExpandFlag == true)
+                    Thread.Sleep(90);
+                else
+                    Thread.Sleep(120);
+                LifeTime -= 1;
+            }
+            if (Stop == true)
+                return;
+            CurrentStars -= 1;
+            return;
+        }
+
+        public 一起去看流星雨()
+        {
+            InitializeComponent();
+        }
+
+        private void 一起去看流星雨_Load(object sender, EventArgs e)
+        {
+            notifyIcon.ContextMenuStrip = 菜单;
+            notifyIcon.ShowBalloonTip(4000);
+
+            mciSendString(@"open " + "song.mp3" + " alias song", null, 0, 0);
+            mciSendString("play song repeat", null, 0, 0);
+            Thread t1 = new Thread(new ThreadStart(ControlMeteors));
+            Thread t2 = new Thread(new ThreadStart(ControlStars));
+            t1.Start();
+            t2.Start();
+        }
+
+        private void ControlMeteors()
+        {
+            Random random = new Random();
+            for (; ; )
+            {
+                if (Stop == false)
+                {
+                    if (CurrentMeterors < MaxMeteors)
+                    {
+                        Thread t = new Thread(new ThreadStart(SingleMeteor));
+                        t.Start();
+                        CurrentMeterors += 1;
+                    }
+                    Thread.Sleep(random.Next(40,60));
+                }
+                else
+                    break;
+            }
+        }
+
+        private void ControlStars()
+        {
+            Random random = new Random();
+            for (; ; )
+            {
+                if (Stop == false)
+                {
+                    if (CurrentStars < MaxStars)
+                    {
+                        Thread t = new Thread(new ThreadStart(SingleStar));
+                        t.Start();
+                        CurrentStars += 1;
+                    }
+                    Thread.Sleep(random.Next(20,40));
+                }
+                else
+                    break;
+            }
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stop = true;
+            notifyIcon.Visible = false;
+            mciSendString("stop song", null, 0, 0);
+            Application.Exit();
+        }
+
+        private void 换个方向ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ClockWise0 == true)
+                ClockWise0 = false;
+            else
+                ClockWise0 = true;
+        }
+
+        private void 让流星雨来得更猛烈些吧ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MaxMeteors += 15;
+        }
+
+        private void 恢复原状ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MaxMeteors = 20;
+        }
+
+        private void 看看满天繁星ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MaxStars = 64;
+        }
+
+        private void 恢复原状ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MaxStars = 32;
+        }
+
+        private void 透明ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.Black;
+        }
+
+        private void 午夜深蓝ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(0, 10, 38);
+        }
+
+        private void 静谧暗灰ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(64, 64, 64);
+        }
+    }
+}
